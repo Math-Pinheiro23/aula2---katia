5d1d299353cfb618f0b2c4fe3954869c68492390
curso.ads <curso.ads@LAB06-28>
2022-08-22 20:53:39 -0300

    Adicionar arquivos de projeto.

---
+++ aula2 - katia/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula2___katia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaração e instanciação de objeto
+            Produto prod = new Produto();
+
+            Console.WriteLine("Digite o codigo do prouduto: ");
+            prod.setCodigo(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a descrição do prouduto: ");
+            prod.setDescricao(Console.ReadLine());
+
+            Console.WriteLine("Digite a categoria do prouduto: ");
+            prod.setCategoria(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do prouduto: ");
+            prod.setValor(int.Parse(Console.ReadLine()));
+            Console.WriteLine();
+
+            Console.WriteLine("Codigo = " + prod.getCodigo() + "\nDescrição: " +
+                prod.getDescricao() + "\nCategoria = " + prod.getCategoria() +
+                "\nValor = " + prod.getValor().ToString("C"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
